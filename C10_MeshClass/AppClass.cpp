6c19176a6ef1a41c2db62af884869a713c2a1399
Nick Altier <nca6109@rit.edu>
2023-02-14 09:27:28 -0500

    commit

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -8,6 +8,16 @@ void Application::InitVariables(void)
 	vector3 v3Upward = AXIS_Y;
 	m_pCameraMngr->SetPositionTargetAndUpward(v3Position, v3Target, v3Upward);
 
+	m_pMyShape = new MyMesh();
+	//These lines do the same as "AddTri()"
+	//m_pMyShape->AddVertexPosition(vector3(0.0f, 0.0f, 0.0f));
+	//m_pMyShape->AddVertexPosition(vector3(1.0f, 0.0f, 0.0f));
+	//m_pMyShape->AddVertexPosition(vector3(0.0f, 1.0f, 0.0f));
+	m_pMyShape->AddTri(vector3(0.0f, 0.0f, 0.0f), vector3(1.0f, 0.0f, 0.0f), vector3(0.0f, 1.0f, 0.0f));
+	m_pMyShape->CompleteMesh(C_BLUE);
+	m_pMyShape->CompileOpenGL3X();
+
+
 	m_pMesh1 = new MyMesh();
 	m_pMesh1->GenerateCube(1.0f, C_PURPLE);
 
@@ -39,9 +49,10 @@ void Application::Display(void)
 	matrix4 m4Model = ToMatrix4(m_qArcBall);
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	m_pMyShape->Render(m4Projection, m4View, m4Model);
 
-	m_pMesh1->Render(m4Projection, m4View, m4Model);
-	m_pMesh2->Render(m4Projection, m4View, glm::translate(IDENTITY_M4, vector3(2.0f)));
+	//m_pMesh1->Render(m4Projection, m4View, m4Model);
+	//m_pMesh2->Render(m4Projection, m4View, glm::translate(IDENTITY_M4, vector3(2.0f)));
 
 	// draw a skybox
 	m_pModelMngr->AddSkyboxToRenderList();
@@ -60,7 +71,11 @@ void Application::Display(void)
 }
 void Application::Release(void)
 {
-
+	if (m_pMyShape != nullptr) //Same as SafeDelete
+	{
+		delete m_pMyShape;
+		m_pMyShape = nullptr;
+	}
 	SafeDelete(m_pMesh1);
 	SafeDelete(m_pMesh2);
 
