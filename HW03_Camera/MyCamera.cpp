1dc0fc95761f7f939c77e5dc811d419425795433
nca6109 <nca6109@rit.edu>
2023-04-02 14:30:04 -0400

    Completed A03

--- HW03_Camera/MyCamera.cpp
+++ HW03_Camera/MyCamera.cpp
@@ -5,7 +5,11 @@ void MyCamera::SetPositionTargetAndUpward(vector3 a_v3Position, vector3 a_v3Targ
 {
 	//TODO:: replace the super call with your functionality
 	//Tip: Changing any positional vector forces you to calculate new directional ones
-	super::SetPositionTargetAndUpward(a_v3Position, a_v3Target, a_v3Upward);
+	m_v3Position = a_v3Position;
+	m_v3Target = a_v3Target;
+	m_v3Upward = a_v3Upward;
+	m_v3Forward = -1 * glm::normalize(a_v3Position);
+	m_v3Rightward = glm::cross(m_v3Forward, m_v3Upward);
 
 	//After changing any vectors you need to recalculate the MyCamera View matrix.
 	//While this is executed within the parent call above, when you remove that line
@@ -21,18 +25,19 @@ void MyCamera::MoveForward(float a_fDistance)
 	//		 in the _Binary folder you will notice that we are moving 
 	//		 backwards and we never get closer to the plane as we should 
 	//		 because as we are looking directly at it.
-	m_v3Position -= vector3(0.0f, 0.0f, a_fDistance);
-	m_v3Target -= vector3(0.0f, 0.0f, a_fDistance);
+	
+	m_v3Position += a_fDistance * m_v3Forward;
+	m_v3Target += a_fDistance * m_v3Forward;
 }
 void MyCamera::MoveVertical(float a_fDistance)
 {
-	m_v3Position -= vector3(0.0f, a_fDistance, 0.0f);
-	m_v3Target -= vector3(0.0f, a_fDistance, 0.0f);
+	m_v3Position += a_fDistance * m_v3Upward;
+	m_v3Target += a_fDistance * m_v3Upward;
 }
 void MyCamera::MoveSideways(float a_fDistance)
 {
-	m_v3Position += vector3(a_fDistance, 0.0f, 0.0f);
-	m_v3Target += vector3(a_fDistance, 0.0f, 0.0f);
+	m_v3Position += a_fDistance * m_v3Rightward;
+	m_v3Target += a_fDistance * m_v3Rightward;
 }
 void MyCamera::CalculateView(void)
 {
@@ -43,10 +48,23 @@ void MyCamera::CalculateView(void)
 	//		 have change so you only need to focus on the directional and positional 
 	//		 vectors. There is no need to calculate any right click process or connections.
 	
-	quaternion q1 = glm::angleAxis(glm::radians(m_v3PitchYawRoll.x), m_v3Rightward);
-	m_v3Target = m_v3Target * glm::toMat4(q1);
-	m_v3Upward += m_v3PitchYawRoll.x;
+	if (m_v3PitchYawRoll != m_v3PrevPitchYawRoll)
+	{
+		//Get quat to hold rotations
+		quaternion orientation = quaternion(vector3(m_v3PitchYawRoll.x, m_v3PitchYawRoll.y, m_v3PitchYawRoll.z));
+
+		//Set directional vectors
+		m_v3Forward = glm::normalize(orientation * vector3(0.0f, 0.0f, -1.0f));
+		m_v3Rightward = glm::normalize(orientation * AXIS_X);
+
+		m_v3Upward = glm::normalize(glm::cross(m_v3Rightward, m_v3Forward));
+	}
+
+	// update target and view based on orientation
+	m_v3Target = m_v3Position + m_v3Forward;
 	m_m4View = glm::lookAt(m_v3Position, m_v3Target, m_v3Upward);
+
+	m_v3PrevPitchYawRoll = m_v3PitchYawRoll;
 }
 //You can assume that the code below does not need changes unless you expand the functionality
 //of the class or create helper methods, etc.
